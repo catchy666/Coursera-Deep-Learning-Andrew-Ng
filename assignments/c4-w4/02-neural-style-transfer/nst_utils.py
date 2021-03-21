6db231adff9b6f8abf8f03b6ddfc1c83b5a49cbe
catchy666 <934326180@qq.com>
2021-03-21 10:43:48 +0800

    update c4w4p2

--- assignments/c4-w4/02-neural-style-transfer/nst_utils.py
+++ assignments/c4-w4/02-neural-style-transfer/nst_utils.py
@@ -7,22 +7,23 @@ import scipy.misc
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 from PIL import Image
-from nst_utils import *
 
 import numpy as np
 import tensorflow as tf
+
 
 class CONFIG:
     IMAGE_WIDTH = 400
     IMAGE_HEIGHT = 300
     COLOR_CHANNELS = 3
     NOISE_RATIO = 0.6
-    MEANS = np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3)) 
-    VGG_MODEL = 'pretrained-model/imagenet-vgg-verydeep-19.mat' # Pick the VGG 19-layer model by from the paper "Very Deep Convolutional Networks for Large-Scale Image Recognition".
-    STYLE_IMAGE = 'images/stone_style.jpg' # Style image to use.
-    CONTENT_IMAGE = 'images/content300.jpg' # Content image to use.
+    MEANS = np.array([123.68, 116.779, 103.939]).reshape((1, 1, 1, 3))
+    VGG_MODEL = 'pretrained-model/imagenet-vgg-verydeep-19.mat'  # Pick the VGG 19-layer model by from the paper "Very Deep Convolutional Networks for Large-Scale Image Recognition".
+    STYLE_IMAGE = 'images/stone_style.jpg'  # Style image to use.
+    CONTENT_IMAGE = 'images/content300.jpg'  # Content image to use.
     OUTPUT_DIR = 'output/'
-    
+
+
 def load_vgg_model(path):
     """
     Returns a model for the purpose of 'painting' the picture.
@@ -75,11 +76,11 @@ def load_vgg_model(path):
         41 is fullyconnected (1, 1, 4096, 1000)
         42 is softmax
     """
-    
+
     vgg = scipy.io.loadmat(path)
 
     vgg_layers = vgg['layers']
-    
+
     def _weights(layer, expected_layer_name):
         """
         Return the weights and bias from the VGG model for a given layer.
@@ -125,42 +126,46 @@ def load_vgg_model(path):
 
     # Constructs the graph model.
     graph = {}
-    graph['input']   = tf.Variable(np.zeros((1, CONFIG.IMAGE_HEIGHT, CONFIG.IMAGE_WIDTH, CONFIG.COLOR_CHANNELS)), dtype = 'float32')
-    graph['conv1_1']  = _conv2d_relu(graph['input'], 0, 'conv1_1')
-    graph['conv1_2']  = _conv2d_relu(graph['conv1_1'], 2, 'conv1_2')
+    graph['input'] = tf.Variable(np.zeros((1, CONFIG.IMAGE_HEIGHT, CONFIG.IMAGE_WIDTH, CONFIG.COLOR_CHANNELS)),
+                                 dtype='float32')
+    graph['conv1_1'] = _conv2d_relu(graph['input'], 0, 'conv1_1')
+    graph['conv1_2'] = _conv2d_relu(graph['conv1_1'], 2, 'conv1_2')
     graph['avgpool1'] = _avgpool(graph['conv1_2'])
-    graph['conv2_1']  = _conv2d_relu(graph['avgpool1'], 5, 'conv2_1')
-    graph['conv2_2']  = _conv2d_relu(graph['conv2_1'], 7, 'conv2_2')
+    graph['conv2_1'] = _conv2d_relu(graph['avgpool1'], 5, 'conv2_1')
+    graph['conv2_2'] = _conv2d_relu(graph['conv2_1'], 7, 'conv2_2')
     graph['avgpool2'] = _avgpool(graph['conv2_2'])
-    graph['conv3_1']  = _conv2d_relu(graph['avgpool2'], 10, 'conv3_1')
-    graph['conv3_2']  = _conv2d_relu(graph['conv3_1'], 12, 'conv3_2')
-    graph['conv3_3']  = _conv2d_relu(graph['conv3_2'], 14, 'conv3_3')
-    graph['conv3_4']  = _conv2d_relu(graph['conv3_3'], 16, 'conv3_4')
+    graph['conv3_1'] = _conv2d_relu(graph['avgpool2'], 10, 'conv3_1')
+    graph['conv3_2'] = _conv2d_relu(graph['conv3_1'], 12, 'conv3_2')
+    graph['conv3_3'] = _conv2d_relu(graph['conv3_2'], 14, 'conv3_3')
+    graph['conv3_4'] = _conv2d_relu(graph['conv3_3'], 16, 'conv3_4')
     graph['avgpool3'] = _avgpool(graph['conv3_4'])
-    graph['conv4_1']  = _conv2d_relu(graph['avgpool3'], 19, 'conv4_1')
-    graph['conv4_2']  = _conv2d_relu(graph['conv4_1'], 21, 'conv4_2')
-    graph['conv4_3']  = _conv2d_relu(graph['conv4_2'], 23, 'conv4_3')
-    graph['conv4_4']  = _conv2d_relu(graph['conv4_3'], 25, 'conv4_4')
+    graph['conv4_1'] = _conv2d_relu(graph['avgpool3'], 19, 'conv4_1')
+    graph['conv4_2'] = _conv2d_relu(graph['conv4_1'], 21, 'conv4_2')
+    graph['conv4_3'] = _conv2d_relu(graph['conv4_2'], 23, 'conv4_3')
+    graph['conv4_4'] = _conv2d_relu(graph['conv4_3'], 25, 'conv4_4')
     graph['avgpool4'] = _avgpool(graph['conv4_4'])
-    graph['conv5_1']  = _conv2d_relu(graph['avgpool4'], 28, 'conv5_1')
-    graph['conv5_2']  = _conv2d_relu(graph['conv5_1'], 30, 'conv5_2')
-    graph['conv5_3']  = _conv2d_relu(graph['conv5_2'], 32, 'conv5_3')
-    graph['conv5_4']  = _conv2d_relu(graph['conv5_3'], 34, 'conv5_4')
+    graph['conv5_1'] = _conv2d_relu(graph['avgpool4'], 28, 'conv5_1')
+    graph['conv5_2'] = _conv2d_relu(graph['conv5_1'], 30, 'conv5_2')
+    graph['conv5_3'] = _conv2d_relu(graph['conv5_2'], 32, 'conv5_3')
+    graph['conv5_4'] = _conv2d_relu(graph['conv5_3'], 34, 'conv5_4')
     graph['avgpool5'] = _avgpool(graph['conv5_4'])
-    
+
     return graph
 
-def generate_noise_image(content_image, noise_ratio = CONFIG.NOISE_RATIO):
+
+def generate_noise_image(content_image, noise_ratio=CONFIG.NOISE_RATIO):
     """
     Generates a noisy image by adding random noise to the content_image
     """
-    
+
     # Generate a random noise_image
-    noise_image = np.random.uniform(-20, 20, (1, CONFIG.IMAGE_HEIGHT, CONFIG.IMAGE_WIDTH, CONFIG.COLOR_CHANNELS)).astype('float32')
-    
+    noise_image = np.random.uniform(-20, 20,
+                                    (1, CONFIG.IMAGE_HEIGHT, CONFIG.IMAGE_WIDTH, CONFIG.COLOR_CHANNELS)).astype(
+        'float32')
+
     # Set the input_image to be a weighted average of the content_image and a noise_image
     input_image = noise_image * noise_ratio + content_image * (1 - noise_ratio)
-    
+
     return input_image
 
 
@@ -168,21 +173,20 @@ def reshape_and_normalize_image(image):
     """
     Reshape and normalize the input image (content or style)
     """
-    
+
     # Reshape image to mach expected input of VGG16
     image = np.reshape(image, ((1,) + image.shape))
-    
+
     # Substract the mean to match the expected input of VGG16
     image = image - CONFIG.MEANS
-    
+
     return image
 
 
 def save_image(path, image):
-    
     # Un-normalize the image so that it looks good
     image = image + CONFIG.MEANS
-    
+
     # Clip and Save the image
     image = np.clip(image[0], 0, 255).astype('uint8')
     scipy.misc.imsave(path, image)
